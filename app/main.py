a7ee308eb25bea878bb982599b200ed1c7242f28
kingjinyong <wlsdydwhswkf@gmail.com>
2025-11-06 11:48:52 +0900

    delete: 모든 파일 삭제

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from app.routes import rag_router, echo_router
-
-app = FastAPI()
-
-# 라우터를 별도 모듈로 분리해 API 구성을 단순화.
-app.include_router(rag_router)
-app.include_router(echo_router)
--- app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rag import router as rag_router
-from .echo import router as echo_router
-
-__all__ = ["rag_router", "echo_router"]
